4daa5c910b6a7850d93c611813bb40eb4670bb9c
Dasa3v <dasaev.cor@gmail.com>
2025-02-25 21:25:10 -0500

    Delete Empresa directory

--- Empresa/static/js/app.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("JavaScript cargado correctamente");
-});
-
-function abrirVentanaLogin() {
-    window.open("/login", "VentanaLogin", "width=500,height=600");
-}
