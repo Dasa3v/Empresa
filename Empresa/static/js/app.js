4e90effa905246bfdccbf306ac6e35d37f21246e
Dasa3v <dasaev.cor@gmail.com>
2025-02-28 20:28:15 -0500

    Add files via upload

---
+++ Empresa/static/js/app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("JavaScript cargado correctamente");
+});
+
+function abrirVentanaLogin() {
+    window.open("/login", "VentanaLogin", "width=500,height=600");
+}
